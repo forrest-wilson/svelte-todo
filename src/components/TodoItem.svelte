f14558afd397c5336f03ddd68b594142c9798cf9
Forrest Wilson-Jennings <forrest.wilson@me.com>
2022-05-20 17:52:08 +1200

    Adding remove button for items

--- src/components/TodoItem.svelte
+++ src/components/TodoItem.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
-	import type { Todo } from '../store';
+	import FlexSpacer from './util/FlexSpacer.svelte';
+	import { todoItems, type Todo } from '../store';
 
 	export let todo: Todo;
+
+	let todos: Todo[];
+
+	todoItems.subscribe((val) => {
+		todos = val;
+	});
+
+	let removeTodo = () => {
+		let idx = todos.findIndex((val) => todo.id === val.id);
+		todos.splice(idx, 1);
+
+		todoItems.set(todos);
+	};
 </script>
 
 <div class="border rounded mb-2">
-	<div class="p-4">
-		<p>{todo.text}</p>
+	<div class="p-4 flex">
+		<div class="flex-initial">
+			<p>{todo.text}</p>
+		</div>
+
+		<FlexSpacer />
+
+		<div class="flex-initial">
+			<button class="text-red-500" on:click={removeTodo}>Remove</button>
+		</div>
 	</div>
 </div>
