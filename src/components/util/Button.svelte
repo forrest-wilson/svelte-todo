005974181a50819027af121eb04671cc919ebee5
Forrest Wilson-Jennings <forrest.wilson@me.com>
2022-05-20 22:29:37 +1200

    Reusable button component

---
+++ src/components/util/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let buttonText: string;
+
+	let cssClasses: string;
+	export { cssClasses as class };
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<button
+	class={`bg-fuchsia-700 text-white font-semibold text-sm px-4 py-2 rounded ${cssClasses || ''}`}
+	on:click={() => dispatch('click')}
+>
+	{buttonText}
+</button>
