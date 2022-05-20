27e2cdc48aabccd14e60042498a2d775ef49532b
Forrest Wilson-Jennings <forrest.wilson@me.com>
2022-05-20 22:29:26 +1200

    Making main container smaller

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,15 @@
-<script>
+<script lang="ts">
 	import Toolbar from '../components/Toolbar.svelte';
 	import TodoList from '../components/TodoList.svelte';
 </script>
 
-<div class="container mx-auto m-4">
+<div class="container mx-auto main-container h-screen">
 	<Toolbar />
 	<TodoList />
 </div>
+
+<style>
+	.main-container {
+		max-width: 600px;
+	}
+</style>
