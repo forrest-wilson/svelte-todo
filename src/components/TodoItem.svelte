b6c77dfa8b7c6237d202d477a35e7a1b5edd59be
Forrest Wilson-Jennings <forrest.wilson@me.com>
2022-05-22 13:13:56 +1200

    More styling

--- src/components/TodoItem.svelte
+++ src/components/TodoItem.svelte
@@ -19,18 +19,20 @@
 </script>
 
 <div
-	class="border border-zinc-100 dark:border-zinc-600 rounded-md shadow-md mb-2 bg-white dark:bg-zinc-800 transition-all"
+	class="border border-zinc-100 dark:border-zinc-600 rounded-md shadow-md mb-2 bg-white dark:bg-zinc-800 transition hover:bg-fuchsia-700 group cursor-pointer hover:scale-[1.05]"
 >
 	<div class="p-4 flex items-center">
 		<div class="flex-initial">
-			<p class="text-gray-700 dark:text-white font-medium">{todo.text}</p>
+			<p class="text-gray-700 dark:text-white group-hover:text-white font-medium transition">
+				{todo.text}
+			</p>
 		</div>
 
 		<FlexSpacer />
 
 		<div class="flex-initial">
 			<button
-				class="text-red-500 hover:text-white hover:bg-red-500 rounded-md px-4 py-2 transition-all"
+				class="text-red-500 hover:text-white hover:bg-red-500 rounded-md px-4 py-2 transition group-hover:text-white"
 				on:click={removeTodo}
 			>
 				<i class="gg-trash" />
