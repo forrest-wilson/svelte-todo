fc399f98a889b9f696c70de17a5dc629e005bd41
Forrest Wilson-Jennings <forrest.wilson@me.com>
2022-05-22 22:28:53 +1200

    Updating to not use a modal

--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -5,13 +5,10 @@
 	import FlexSpacer from './util/FlexSpacer.svelte';
 	import { appState } from '../stores/appState';
 	import RoundedSwitch from './util/RoundedSwitch.svelte';
-	import Modal from './util/Modal.svelte';
-	import { modalState } from '../stores/modalState';
 
 	let newTodo = '';
 	let todos: Todo[];
 	let darkMode: boolean;
-	let dialog: boolean;
 
 	// Subscribers
 	todoItems.subscribe((val) => {
@@ -22,19 +19,14 @@
 		darkMode = val.darkMode;
 	});
 
-	modalState.subscribe((val) => {
-		dialog = val;
-	});
-
 	// Reactive watchers
-	$: if (!dialog) newTodo = '';
 	$: appState.set({ darkMode });
 
 	// Methods
 	const addTask = () => {
 		todoItems.set([...todos, { id: v4(), text: newTodo, isCompleted: false }]);
 
-		setModalState(false);
+		newTodo = '';
 	};
 
 	const handleKeypress = (keypress: KeyboardEvent) => {
@@ -42,65 +34,30 @@
 			case 'Enter':
 				addTask();
 				break;
-			case 'Escape':
-				setModalState(false);
-				break;
-			case 'a':
-				if (!dialog) {
-					keypress.preventDefault();
-					setModalState(true);
-				}
-
-				break;
 		}
 	};
 
 	const handleCheck = (evt: CustomEvent) => {
 		darkMode = evt.detail;
 	};
-
-	const setModalState = (state: boolean) => {
-		modalState.set(state);
-	};
 </script>
 
-<svelte:window on:keydown={handleKeypress} />
-
-<div class="flex flex-row mb-4 border-b-2 pb-2 items-center">
-	<Button
-		buttonText="Add Todo"
-		on:click={() => setModalState(true)}
-		class="mr-2 shadow-md transition"
-	/>
-
-	<div class="text-purple-500"><b>Press A</b></div>
-
+<div class="flex pb-2 items-center">
 	<FlexSpacer />
 
 	<RoundedSwitch state={darkMode} on:checked={handleCheck} />
 </div>
 
-<Modal>
-	<div class="flex">
-		<FlexSpacer />
-		<button on:click={() => setModalState(false)}>x</button>
-	</div>
+<div class="flex mb-4 items-center">
+	<!-- svelte-ignore a11y-autofocus -->
+	<input
+		on:keyup={handleKeypress}
+		bind:value={newTodo}
+		placeholder="Enter todo"
+		type="text"
+		class="mr-2 w-full rounded-md"
+		autofocus
+	/>
 
-	<div class="pb-4">
-		<!-- svelte-ignore a11y-autofocus -->
-		<input
-			on:keyup={handleKeypress}
-			bind:value={newTodo}
-			placeholder="Enter todo"
-			type="text"
-			class="mt-1 block w-full rounded-md"
-			autofocus
-		/>
-	</div>
-
-	<Button buttonText="Add" class="w-full" on:click={addTask} />
-
-	<div class="block p-2 text-center text-gray-700 font-light text-sm">
-		<span>Press Enter to add</span>
-	</div>
-</Modal>
+	<Button buttonText="Add Todo" on:click={addTask} />
+</div>
--- src/components/util/Button.svelte
+++ src/components/util/Button.svelte
@@ -3,14 +3,14 @@
 
 	export let buttonText: string;
 
-	let cssClasses: string;
+	let cssClasses: string = '';
 	export { cssClasses as class };
 
 	const dispatch = createEventDispatcher();
 </script>
 
 <button
-	class={`bg-fuchsia-700 text-white font-semibold text-sm px-4 py-2 rounded-md ${cssClasses || ''}`}
+	class={`bg-fuchsia-700 text-white font-semibold text-sm px-4 py-2 rounded-md ${cssClasses}`}
 	on:click={() => dispatch('click')}
 >
 	{buttonText}
